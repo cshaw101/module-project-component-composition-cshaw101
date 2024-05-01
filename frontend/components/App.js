1485a034a13e07ec3165d1b3c61c9250265e30d6
Caleb Shaw <124619458+cshaw101@users.noreply.github.com>
2024-04-30 21:28:49 -0400

    data fetched and state set

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 function App() {
+let [data, setData] = useState(null)
+
+useEffect(() => {
+axios.get('https://api.nasa.gov/planetary/apod?api_key=m6Xi96z1LeOaZibC7GlfKrqMa43YVuPfdjSHgvxp')
+     .then((res) => {
+      console.log(res.data)
+      setData(res.data)
+     })
+     .catch((err) => {
+      console.error(err)
+     })
+}, [])
+
   return (
     <p>
       Read through the instructions in the README.md file to build your NASA
