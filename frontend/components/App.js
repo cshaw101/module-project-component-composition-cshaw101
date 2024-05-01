c853dabe0f197648dd19c97966c53ee68cd9a072
Caleb Shaw <124619458+cshaw101@users.noreply.github.com>
2024-04-30 21:54:49 -0400

    fixed state

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -6,13 +6,11 @@ import Footer from './Footer'
 
 function App() {
 let [data, setData] = useState(null)
-let [photo, setPhoto] = useState(null)
 
 useEffect(() => {
 axios.get('https://api.nasa.gov/planetary/apod?api_key=m6Xi96z1LeOaZibC7GlfKrqMa43YVuPfdjSHgvxp')
      .then((res) => {
       setData(res.data)
-      setPhoto(res.data.hdurl)
       console.log(res.data)
      })
      .catch((err) => {
@@ -22,7 +20,7 @@ axios.get('https://api.nasa.gov/planetary/apod?api_key=m6Xi96z1LeOaZibC7GlfKrqMa
 
   return (
     <>
-    <Image imageData={photo} />
+    <Image imageData={data?.hdurl} />
    <Body bodyData={data?.explanation} />
     <Footer footerData={data?.date} />
     </>
